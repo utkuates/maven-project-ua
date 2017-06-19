659f513dcb14409b072c39e3e34ffe30d98d9a90
Utku Ates <utku.ates@hotmail.com.tr>
2017-06-19 15:04:11 +0300

    Egit example

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,6 +8,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
+        
     }
     
     @Override
